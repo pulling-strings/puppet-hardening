9d6f958316775b38f7273dd9297d6ac5c6ea3eb1
ronen <narkisr@gmail.com>
2014-11-05 20:45:25 +0200

    should be sshd

--- manifests/ssh.pp
+++ manifests/ssh.pp
@@ -2,22 +2,22 @@
 #  http://bodhizazen.net/Tutorials/SSH_security 
 class hardening::ssh($user,$strict=false){
   file_line { 'disable ssh protocol 1':
-    path => '/etc/ssh/ssh_config',
+    path => '/etc/ssh/sshd_config',
     line => 'Protocol 2'
   }
 
   file_line {'Disable password auth' :
-    path => '/etc/ssh/ssh_config',
+    path => '/etc/ssh/sshd_config',
     line => 'PasswordAuthentication no'
   }
 
   file_line {'Allow only single ssh user' :
-    path => '/etc/ssh/ssh_config',
+    path => '/etc/ssh/sshd_config',
     line => "AllowUsers ${user}"
   }
 
   file_line {'Dont allow root ssh' :
-    path => '/etc/ssh/ssh_config',
+    path => '/etc/ssh/sshd_config',
     line =>  'PermitRootLogin no'
   } ~> Service['ssh']
 
@@ -25,17 +25,17 @@ class hardening::ssh($user,$strict=false){
 
   if($strict){
     file_line {'No tcp forwarding' :
-      path => '/etc/ssh/ssh_config',
+      path => '/etc/ssh/sshd_config',
       line => 'AllowTcpForwarding no'
     }
 
     file_line {'No X11 forwarding' :
-      path => '/etc/ssh/ssh_config',
+      path => '/etc/ssh/sshd_config',
       line => 'X11Forwarding no'
     }
 
     file_line {'Strict file mode checks' :
-      path => '/etc/ssh/ssh_config',
+      path => '/etc/ssh/sshd_config',
       line => 'StrictModes yes'
     }
   }
